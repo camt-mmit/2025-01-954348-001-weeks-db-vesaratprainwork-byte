a861e946b1982642baa7a50c6527b621c382a632
john doe <johndoe@ecample.com>
2025-10-03 23:27:38 +0700

    แก้อะไรก็ไม่รู้

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -18,13 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'email',
         'password',
     ];
-
-    public function isAdministrator(): bool
-    {
-        return $this->role === 'ADMIN';
-    }
 
     /**
      * The attributes that should be hidden for serialization.
